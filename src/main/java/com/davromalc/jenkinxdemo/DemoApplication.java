d4c46ea81064bb44f8d28cbbb27a93761923db82
David Romero Alcaide <david.romero4.contractor@bbva.com>
2018-07-08 14:04:53 +0200

    fix:(plugins) use a better version of maven deploy plugin

--- src/main/java/com/davromalc/jenkinxdemo/DemoApplication.java
+++ src/main/java/com/davromalc/jenkinxdemo/DemoApplication.java
@@ -3,10 +3,14 @@ package com.davromalc.jenkinxdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		log.info("Running Demo with jenkins X");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
